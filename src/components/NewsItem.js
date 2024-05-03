71b5a77e04521c5681fad7f08926904f78478bdd
umarbsse <umarbsse@gmail.com>
2024-05-03 12:32:38 +0500

    ading multiple news images work

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let {title, descripition} = this.props;
+    let {title, descripition, imageUrl, newsUrl} = this.props;
     return (
       <div>
-            <div class="card" style= {{width: "18rem"}}>
-                <img src="https://cdn.theathletic.com/app/uploads/2024/05/01215907/GettyImages-2150483840-scaled.jpg" class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">{title}</h5>
-                    <p class="card-text">{descripition}</p>
-                    <a href="/newsitem" class="btn btn-primary">Go somewhere</a>
+            <div className="card" style= {{width: "18rem"}}>
+                <img src={imageUrl} className="card-img-top" alt="...." />
+                <div className="card-body">
+                    <h5 className="card-title">{title}</h5>
+                    <p className="card-text">{descripition}</p>
+                    <a href="/newsdetail/" className="btn btn-sm btn-primary">Read More</a>
                 </div>
             </div>
       </div>
