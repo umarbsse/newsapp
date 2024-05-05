837f132ed03ddeadd84e0ecdd3cf96e73acf31f4
umarbsse <umarbsse@gmail.com>
2024-05-06 00:31:05 +0500

    done category wise Navbar

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
-
+import{BrowserRouter,
+  Routes,
+  Route,
+  }from "react-router-dom";
 export default class App extends Component {
   render() {
     return (
-      <div>
+      
+    <>
+    <BrowserRouter>
         <Navbar/>
-        <News pageSize={5} country="us" category="sports" />
-      </div>
+         <Routes>
+          <Route path="/" element={<News key="general" pageSize={5} country="us" category="general" />} />
+          <Route path="/business" element={<News key="business" pageSize={5} country="us" category="business" />} />
+          <Route path="/entertainment" element={<News key="entertainment" pageSize={5} country="us" category="entertainment" />} />
+          <Route path="/general" element={<News key="general" pageSize={5} country="us" category="general" />} />
+          <Route path="/health" element={<News key="health" pageSize={5} country="us" category="health" />} />
+          <Route path="/science" element={<News key="science" pageSize={5} country="us" category="science" />} />
+          <Route path="/sports" element={<News key="sports" pageSize={5} country="us" category="sports" />} />
+          <Route path="/technology" element={<News key="technology" pageSize={5} country="us" category="technology" />} />
+        </Routes>
+      </BrowserRouter> 
+    </>
     )
   }
 }
